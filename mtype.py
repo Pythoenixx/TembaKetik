7942f34b48bd75667d7786891c5ade3cd946c237
Pythoenixx <azanyzikry25@gmail.com>
2023-11-05 11:36:54 +0800

    sblm tukar jdi class

--- mtype.py
+++ mtype.py
@@ -1,5 +1,4 @@
-import pygame
-import sys
+import pygame,sys,random
 from pemalar import *
 #klo penembak nya kat tgh pun agak interesting
 # Initialize pygame
@@ -11,9 +10,16 @@ clock = pygame.time.Clock()
 # Set the title of the window
 pygame.display.set_caption("Shootype")
 
+class Latar:
+    def __init__(self, x, y, veloX, veloY) -> None:
+        self.x = x
+        self.y = y
+        self.veloX = veloX
+        self.veloY = veloY
+latar_x,latar_y = 0,WN_TINGGI
 latar = pygame.image.load('img/angkasa1.png').convert_alpha()
+
 latar = pygame.transform.scale(latar,(WN_LEBAR*10,WN_TINGGI*10))
-latar_x,latar_y = 0,0
 
 
 # Get the center of the screen
@@ -26,9 +32,6 @@ pygame.draw.line(screen, (255, 255, 255), (center_x - 100, center_y), (center_x 
 # Create a rectangle object with a position of (50, 50) and a size of (100, 100)
 rect = pygame.Rect(0, 0, 40, 40)
 rect.center = (center_x, WN_TINGGI - 100)
-
-# Draw a red square on the screen using the rectangle object
-pygame.draw.rect(screen, (255, 0, 0), rect)
 
 # Create a game loop
 running = True
@@ -49,10 +52,25 @@ while running:
             x, y = pygame.mouse.get_pos()
             # Print the mouse coordinates
             print(x, y)
-    latar_x -= 0
-    latar_y += 1
+            
+    kotak_latar = latar.get_rect(midbottom=(latar_x,latar_y))
     screen.fill((0, 0, 0))
-    screen.blit(latar, (latar_x,latar_y))
+    screen.blit(latar, kotak_latar)
+    
+    latar_y += 1
+    # Check if the image goes off the bottom of the screen
+    if latar_y > (latar.get_height()):
+        # Wrap the image around to the top of the screen
+        latar_y -= latar.get_height() - WN_TINGGI
+        latar_x += 100
+        if latar_x > (latar.get_width()):
+            latar_x = random.randint(0,latar.get_width())
+    
+    print("coord Y",latar_y)
+    # print(latar.get_size()) (5000, 8200)
+    
+    # Draw a red square on the screen using the rectangle object
+    pygame.draw.rect(screen, (255, 0, 0), rect)
     
     pygame.display.flip()
     clock.tick(FPS)
