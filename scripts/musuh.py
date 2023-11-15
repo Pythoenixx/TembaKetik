7d6ec90bd4b856f8c181901e39192b4ef7645f12
Pythoenixx <azanyzikry25@gmail.com>
2023-11-15 21:40:27 +0800

    revamp the data struct and inheritance

--- scripts/musuh.py
+++ scripts/musuh.py
@@ -1,16 +1,18 @@
 import pygame,random
 
-class Musuh:
-    def __init__(self, x, y, w, h, color, target, speed) -> None:
-        self.rect = pygame.Rect(0, 0, w, h)
+class Musuh(pygame.sprite.Sprite):
+    def __init__(self, x, y, image_path, target, speed) -> None:
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.gerakanX,self.gerakanY = self.rect.x,self.rect.y #kene buat lagi satu variables coords sbb coords yg kat rect pygame x blh jdi float so akan ada rounding error
-        self.color = color
         self.direction = pygame.math.Vector2(target.center) - pygame.math.Vector2(self.rect.center)
         self.direction = self.direction.normalize() # unit vector
         self.speed = speed
         
-    def update(self):
+    def update(self, WN_TINGGI):
         # Update the position of the rect
         self.gerakanX +=  (self.speed * self.direction.x)
         self.gerakanY +=  (self.speed * self.direction.y)
@@ -18,38 +20,22 @@ class Musuh:
         self.rect.x = self.gerakanX
         self.rect.y = self.gerakanY
         
-    def jana(bilangan, WN_LEBAR, pemain_rect):
-        list_musuh = []
-        for i in range(bilangan):
-            # Generate random coordinates, size, and color for each rect
-            x = random.randint(0, WN_LEBAR)
-            y = random.randint(-100, 69)
-            w = random.randint(10, 50)
-            h = w #sbb nk buat square
-            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            laju = random.uniform(0.5, 10)
-            
-            musuh = Musuh(x, y, w, h, color, pemain_rect, laju)
-            list_musuh.append(musuh)
-            
-        return list_musuh
+        self.destruct(WN_TINGGI)
         
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+    def destruct(self, WN_TINGGI):
+        if self.gerakanY > WN_TINGGI + 69:
+            self.kill()
 
-class Pengawal_Musuh:
-    def __init__(self) -> None:
-        self.list_musuh = []
+def jana_musuh(bilangan, WN_LEBAR, pemain_rect):
+    musuh_group = pygame.sprite.Group()
+    for i in range(bilangan):
+        # Generate random coordinates, size, and color for each rect
+        x = random.randint(0, WN_LEBAR)
+        y = random.randint(-100, 69)
+        laju = random.uniform(0.5, 10)
         
-    def jana(self, bilangan, WN_LEBAR, pemain_rect):
-        for i in range(bilangan):
-            # Generate random coordinates, size, and color for each rect
-            x = random.randint(0, WN_LEBAR)
-            y = random.randint(-100, 69)
-            w = random.randint(10, 50)
-            h = w #sbb nk buat square
-            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            laju = random.uniform(0.5, 10)
-            
-            musuh = Musuh(x, y, w, h, color, pemain_rect, laju)
-            self.list_musuh.append(musuh)
+        musuh = Musuh(x, y, 'img/tahi_bintang.png', pemain_rect, laju)
+        musuh_group.add(musuh)
+        
+    return musuh_group
+
